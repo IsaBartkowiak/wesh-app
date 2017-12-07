69366bbb628dae8f414dc7f20a667c9590e7021b
Isa <isa@MacBook-Pro-de-Isa.local>
2017-12-07 20:55:02 +0100

    CORS

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ var routes = require('./routes/index.js');
 var app = express();
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
